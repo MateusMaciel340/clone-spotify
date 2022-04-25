9ebd593335a65273f0259fa3c89b3f3cf7a6504b
Mateus Sousa <mateusmaciel340@gmail.com>
2022-04-25 19:54:02 -0300

    Inserindo dados no array de listas | Link do fontawesome no index

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -1,4 +1,4 @@
-import { lista_menu } from "../types/lista-menu";
+import { lista_menu, lista_footer } from "../types/listas";
 
 export const listagem_menu: lista_menu[] = [
     { titulo: "Premium", link: "/premium" }, 
@@ -7,4 +7,22 @@ export const listagem_menu: lista_menu[] = [
     { titulo: "|", link: "/" },
     { titulo: "Increver-se", link: "/increver-se" },
     { titulo: "Entrar", link: "/entrar" }
+]
+
+export const listagem_footer_empresa: lista_footer[] = [
+    { lista: "Sobre" }, { lista: "Empregos" }, { lista: "For the Record" }
+]
+
+export const listagem_footer_comunidade: lista_footer[] = [
+    { lista: "Para Artistas" }, { lista: "Desenvolvedores" }, { lista: "Publicidade" },
+    { lista: "Publicidade" }, { lista: "Investidores" }, { lista: "Fornecedores" }
+]
+
+export const listagem_footer_links_uteis: lista_footer[] = [
+    { lista: "Suporte" }, { lista: "Player da Web" }, { lista: "Aplicativo móvel grátis" }
+]
+
+export const listagem_icones: lista_footer[] = [
+    { lista: "fa-2x fa fa-instagram" }, { lista: "fa-2x fa fa-twitter" },
+    { lista: "fa-2x fa fa-facebook" }
 ]
