dd187be21896a8d70f0032c5b229577e80dc0e94
Mateus Sousa <mateusmaciel340@gmail.com>
2022-04-25 15:47:25 -0300

    Rota inicial | Fonte sans-serif | imagem logo do spotify

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,14 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Header - Footer
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
 // Pages 
 import Inicial from "../pages";
 
 function Rotas(){
     return(
         <BrowserRouter>
+            <Header/>
             <Routes>
                 <Route path="/" element={<Inicial/>}/>
             </Routes>
+            <Footer/>
         </BrowserRouter>
     )
 }
--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -3,5 +3,6 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
     body{
         padding: 0; margin: 0; box-sizing: border-box;
+        font-family: sans-serif;
     }
 `;
