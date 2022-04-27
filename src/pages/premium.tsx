45916ed4c39fcfa7472439148ec45ebea9214fef
Mateus Sousa <mateusmaciel340@gmail.com>
2022-04-27 14:14:06 -0300

    Inserindo estrutura da page premium | adicionando imagens de premium

--- src/pages/premium.tsx
+++ src/pages/premium.tsx
@@ -1,13 +1,9 @@
 import * as C from "../components/Premium/style";
 import * as Home from "../components/Home/style";
 import ImagemHome from "../assets/home/imagem-home.png";
-
-import { listagem_home_superior } from "../data/listagem";
-import { useState } from "react";
+import { listagem_home_superior, listagem_informacoes } from "../data/listagem";
 
 function Premium(){
-
-    const [listagemPremiumSuperior, setListagemPremiumSuperior] = useState(listagem_home_superior);
 
     return(
         <>
@@ -15,9 +11,9 @@ function Premium(){
                 <C.CorpoSuperior>
                     {listagem_home_superior.map((listagem_premium_superior, index) => (
                         listagem_premium_superior.id === 3 ? (
-                        <Home.Coluna>
+                        <Home.Coluna key={index}>
                             <Home.TextoPrincipal tamanho_titulo="40px">{listagem_premium_superior.titulo}</Home.TextoPrincipal>
-                            <Home.TextoParagrafo>{listagem_premium_superior.paragrafo}</Home.TextoParagrafo>
+                            <Home.TextoParagrafo tamanho_paragrafo="20px">{listagem_premium_superior.paragrafo}</Home.TextoParagrafo>
 
                             <C.DivisaoBotoes>
                                 <Home.BotaoHome borda="#000000">{listagem_premium_superior.texto_botao}</Home.BotaoHome>
@@ -32,6 +28,22 @@ function Premium(){
                     <Home.Coluna>
                         <Home.HomeImagem src={ImagemHome}/>
                     </Home.Coluna>
+                </C.CorpoSuperior>
+            </C.CorpoPremium>
+
+            <C.CorpoPremium fundo="#FFFFFF">
+                <C.CorpoSuperior fundo="#FFFFFF" cor="#000000" grid="">
+                    <Home.TextoPrincipal tamanho_titulo="40px" tipo_titulo="center">Por que ser Premium?</Home.TextoPrincipal>
+
+                    <C.GrupoInformacoes>
+                        {listagem_informacoes.map((dado, index) => (
+                            <C.Informacoes key={index}>
+                                <Home.HomeImagem src={dado.imagem} width="50%"/>
+                                <Home.TextoPrincipal tamanho_titulo="21px">{dado.titulo}</Home.TextoPrincipal>
+                                <Home.TextoParagrafo tamanho_paragrafo="16px" negrito="">{dado.paragrafo}</Home.TextoParagrafo>
+                            </C.Informacoes>
+                        ))}
+                    </C.GrupoInformacoes>
                 </C.CorpoSuperior>
             </C.CorpoPremium>
         </>
