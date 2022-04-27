58160197319c0ab6d287b375637ebcefd38ce554
Mateus Sousa <mateusmaciel340@gmail.com>
2022-04-27 14:14:31 -0300

    Adicionando array de informações | tipificação delas

--- src/data/listagem.ts
+++ src/data/listagem.ts
@@ -1,4 +1,10 @@
-import { lista_home } from "../types/listas";
+import { lista_home, lista_informacoes } from "../types/listas";
+
+// Assets - Imagens
+import ModoOffline from "../assets/premium/coluna-1-musica.png";
+import Sem_Anuncios from "../assets/premium/coluna-2-celular.png";
+import Ouca_Onde_Quiser from "../assets/premium/coluna-3-sons.png";
+import Play from "../assets/premium/coluna-4-play.png";
 
 export const listagem_home_superior: lista_home[] = [
     { 
@@ -24,5 +30,29 @@ export const listagem_home_superior: lista_home[] = [
         paragrafo: "Começa hoje a ouvir música sem anúncios e offline grátis até 27 de julho de 2022. Cancela quando quiseres.",
         texto_botao: "aproveite 3 meses grátis",
         texto_mini: "Somente no plano Individual. Depois, é só R$ 19,90/mês. Sujeito a Termos e Condições. Disponível apenas para quem nunca usou o Premium. Esta oferta termina em 12 de maio de 2022."
+    }
+]
+
+export const listagem_informacoes: lista_informacoes[] = [
+    {
+        id: 1,
+        imagem: ModoOffline,
+        titulo: "Modo offline.",
+        paragrafo: "Ouça música onde estiver."
+    },{
+        id: 2,
+        imagem: Sem_Anuncios,
+        titulo: "Sem anúncios.",
+        paragrafo: "Curta música, sem parar.",
+    },{
+        id: 3,
+        imagem: Ouca_Onde_Quiser,
+        titulo: "Ouça o que quiser.",
+        paragrafo: "Mesmo no celular ou tablet."
+    },{
+        id: 4,
+        imagem: Play,
+        titulo: "Troque de música o quanto quiser.",
+        paragrafo: "Pule quando quiser."
     }
 ]
--- src/types/listas.ts
+++ src/types/listas.ts
@@ -10,3 +10,7 @@ export type lista_home = {
     id: number, titulo_pequeno: string, titulo: string, paragrafo: string,
     texto_botao: string, texto_mini: string
 }
+
+export type lista_informacoes = {
+    id: number, imagem: string, titulo: string, paragrafo: string,
+}
