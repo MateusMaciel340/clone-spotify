fbb9670fbb095c9367907089bb2bb6fba4340050
Mateus Sousa <mateusmaciel340@gmail.com>
2022-04-28 08:19:29 -0300

    Estilização da page download | inserindo imagens de instalação

---
+++ src/components/Baixar/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const CorpoBaixar = styled.div`
+    max-width: 900px; margin: 0 auto;
+    margin-top: 100px; margin-bottom: 50px;
+    padding: 10px;
+`;
